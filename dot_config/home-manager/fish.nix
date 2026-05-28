711b9984f3a07bae25fcc10f9bd19604d773cc7c
Dylan Grousson <59664001+diaarca@users.noreply.github.com>
2026-05-28 11:26:22 +0200

    added multicd function

--- dot_config/home-manager/fish.nix
+++ dot_config/home-manager/fish.nix
@@ -5,13 +5,13 @@
     enable = true;
 
     interactiveShellInit = ''
-      # Source personal fish config if it exists
+      # source personal fish config if it exists
       if test -f ~/.fishrc
         source ~/.fishrc
       end
 
-      # Dynamically create aliases for git project directories
-      set git_dir "$HOME/Documents/git"
+      # dynamically create aliases for git project directories
+      set git_dir "$HOME/git"
       if test -d "$git_dir"
         for dir in "$git_dir"/*
           if test -d "$dir"
@@ -21,10 +21,10 @@
         end
       end
 
-      set fish_greeting # Disable greeting
       if command -q nix-your-shell
         nix-your-shell fish | source
       end
+
       set -gx pure_enable_single_line_prompt true
       set -gx pure_enable_nixdevshell true
       set -gx pure_enable_virtualenv true
@@ -41,6 +41,10 @@
         src = pkgs.fishPlugins.pure.src;
       }
     ];
+
+    functions = {
+      multicd = "echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)";
+    };
 
     shellAbbrs = {
       dotdot = {
