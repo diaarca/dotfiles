d1c15926d8950175972b188e378a0759868c6eef
Dylan Grousson <59664001+diaarca@users.noreply.github.com>
2026-05-28 11:29:31 +0200

    added many git related aliases

--- dot_config/home-manager/aliases.nix
+++ dot_config/home-manager/aliases.nix
@@ -10,5 +10,13 @@ in
     icat = "kitten icat";
     kitty = if isLinux then "nixGL kitty" else "kitty";
     nv = "nvim";
+    gst = "git status";
+    gcm = "git commit";
+    gps = "git push";
+    gdf = "git diff";
+    gsw = "git switch";
+    gad = "git add";
+    gpl = "git pull";
+    gft = "git fetch";
   };
 }
